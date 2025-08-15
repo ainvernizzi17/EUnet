88bf4655f16888779518264d2dbab812485d34d0
ainvernizzi17 <alessia.invernizzi@eup.gess.ethz.ch>
2025-08-15 16:47:49 +0200

    Delete R directory

--- R/merge_load.R
+++
@@ -1,8 +0,0 @@
-#' Load EU dyadic dataset
-#'
-#' @return A data.frame with EU variables
-#' @export
-load_euvar <- function() {
-  system.file("data", "euvar.rds.xz", package = "EUnet") %>%
-    readRDS()
-}
