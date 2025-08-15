056b1a958cc89014e13912067ecd53156503e870
ainvernizzi17 <alessia.invernizzi@eup.gess.ethz.ch>
2025-08-15 17:56:50 +0200

    Delete R directory

--- R/load.R
+++
@@ -1,9 +0,0 @@
-#' Load EU dyadic dataset
-#'
-#' @return A data.frame with EU variables
-#' @export
-load_euvar <- function() {
-  # Use `system.file()` only if needed, but here lazy data can be loaded with `data()`
-  utils::data("euvar", package = "EUnet", envir = environment())
-  return(euvar)
-}
